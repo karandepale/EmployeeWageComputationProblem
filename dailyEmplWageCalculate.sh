048ca7f8129b68584c560592c17100f3afb90c3e
“karandepale” <karandepale111@gmail.com>
2023-05-05 11:09:28 -0700

    Add: part time employee wage Calculation

--- dailyEmplWageCalculate.sh
+++ dailyEmplWageCalculate.sh
@@ -1,4 +1,8 @@
 wagePerHr=20;
 fullDayHr=8;
+partTimeHr=4;
 dailyWage=$((wagePerHr*fullDayHr))
+partTimeWage=$((wagePerHr*partTimeHr))
+
 echo "Daily Employee Wage : " $dailyWage;
+echo "Part Time Employee Wage :" $partTimeWage;
