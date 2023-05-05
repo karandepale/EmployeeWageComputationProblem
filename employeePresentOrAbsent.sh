e18f2c4f47961ef756da81b79bbdf4383eae113e
“karandepale” <karandepale111@gmail.com>
2023-05-05 10:51:10 -0700

    update :present or absent code

--- employeePresentOrAbsent.sh
+++ employeePresentOrAbsent.sh
@@ -1,1 +1,10 @@
-echo "Present or Absent"
+isPresent=1
+isAbsent=0
+check=$((RANDOM%2))
+
+if [ $check -eq $isPresent ]
+then
+  echo "Employee is present" $check
+else
+  echo "Employee is Absent" $check
+fi
